d9fca19c54163601b0921523a1eb1768c1982d42
varshapanda <varsha.vijayashwini@kalvium.community>
2025-08-06 11:23:33 +0530

    Implement user authentication with register and login functionality

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/database.js');
+const authRoutes = require('./routes/authRoutes.js')
+
 
 const app = express();
 connectDB();
@@ -13,6 +15,8 @@ app.use(express.json());
 app.get('/test', (req, res)=>{
     return res.status(200).send("Welcome to LearnLoom - Weaving your learning experience");
 })
+
+app.use('/auth', authRoutes);
 
 const PORT = process.env.PORT || 8080;
 
---
+++ BACKEND/model/user.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true, "Name is required"],
+        trim:true,
+        minlengt:[2, "Name must be at least 2 characters"],
+    },
+    email:{
+        type:String,
+        unique:true,
+        required:[true, "Email is required"],
+        trim:true,
+        lowercase:true,
+        match:[/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, "Please enter a valid email address"]
+    },
+    password:{
+        type:String,
+        required:[true, "Password is required"],
+        minlength:[6, "Password must be at least 6 characters"]
+    },
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
